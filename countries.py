f8a7db03c78454ec574ac6731e2b6c4decf22b7e
AiswaryaDinil <106660088+AiswaryaDinil@users.noreply.github.com>
2022-07-05 11:30:46 +0530

    Add files via upload

--- countries.py
+++ countries.py
@@ -5,18 +5,42 @@ with open("countries.json",encoding="utf-8") as f:
     data=json.load(f)
 print(len(data))
 
+def get_country_data(country_name):
+    return [country for country in data if country["name"].lower().startswith(country_name)]
 # print lang of Ukraine
 
-lang_ukraine=[lang["languages"] for lang in data if lang["name"]=="Ukraine"]
-print(lang_ukraine[0])
+lang_country=[lang["languages"] for lang in data if lang["name"]=="India"]
+languages=[lang["name"] for lang in lang_country[0]]
+print(languages)
 
 # print currency in china
 
-currency_china=[cur["currencies"] for cur in data if cur["name"]=="China"]
-print(currency_china[0])
+currency_country=[cur["currencies"] for cur in data if cur["name"]=="Western Sahara"]
+currency=[cur["name"] for cur in currency_country[0]]
+print(currency)
 
 # print population of india
 
 india_popu=[p["population"] for p in data if p["name"]=="India"]
 print(india_popu)
+
+# print neighbouring countries
+
+country_data=get_country_data("france")
+# print(country_data)
+country_borders=country_data[0].get("borders")
+border_of_india=[country["name"] for country in data if country["alpha3Code"] in country_borders]
+print(border_of_india)
+
+# highest population
+
+# high_popul=max([country["population"] for country in data])
+# print(high_popul)
+populated_country=max(data,key=lambda d:d.get("population"))
+print(populated_country["name"])
+
+# lowest population
+
+low_populated_country=min(data,key=lambda d:d.get("population"))
+print(low_populated_country["name"])
 
---
+++ handling.py
@@ -0,0 +1,4 @@
+# try
+# expect
+# raise
+# finally
