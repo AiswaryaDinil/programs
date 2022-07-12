af2cf2406adddf4ac1b4c3b97ce1c43619816a6f
AiswaryaDinil <106660088+AiswaryaDinil@users.noreply.github.com>
2022-07-12 11:29:14 +0530

    Add files via upload

---
+++ form_largenum.py
@@ -0,0 +1,8 @@
+from functools import reduce
+lst=[25,30,34,5,9]
+str_lst=[str(n) for n in lst]
+print(str_lst)
+
+max_num=reduce(lambda n1,n2:(n1+n2) if (n1+n2)>(n2+n1) else (n2+n1),str_lst)
+print(sorted(max_num))
+
